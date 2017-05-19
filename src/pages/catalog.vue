e63ce7fc5a2e96afa3716ddb181148e31a561e6d
ericwang1902 <2248930513@qq.com>
2017-05-19 18:00:36 +0800

    first

---
+++ src/pages/catalog.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <group title="产品目录">
+            <cell title="存款" is-link :border-intent="false" :arrow-direction="showContent001 ? 'up' : 'down'" @click.native="showContent001 = !showContent001"></cell>
+            <template class="slide" :class="showContent001?'animate':''" v-if="showContent001">
+                <cell-box :border-intent="false" class="sub-item" is-link>个人</cell-box>
+                <cell-box class="sub-item" is-link>企业</cell-box>
+            </template>
+
+
+            <cell title="理财" is-link :border-intent="false" :arrow-direction="showContent002 ? 'up' : 'down'" @click.native="showContent002 = !showContent002"></cell>
+            <template class="slide" :class="showContent002?'animate':''" v-if="showContent002">
+                <cell-box :border-intent="false" class="sub-item" is-link>个人</cell-box>
+                <cell-box class="sub-item" is-link>企业</cell-box>
+            </template>
+
+
+
+            <cell title="中间业务" is-link :border-intent="false" :arrow-direction="showContent003 ? 'up' : 'down'" @click.native="showContent003 = !showContent003"></cell>
+            <template class="slide" :class="showContent003?'animate':''" v-if="showContent003">
+                <cell-box :border-intent="false" class="sub-item" is-link>代缴类</cell-box>
+                <cell-box class="sub-item" is-link>贵金属</cell-box>
+            </template>
+
+            <cell title="电子银行" is-link :border-intent="false" :arrow-direction="showContent004 ? 'up' : 'down'" @click.native="showContent004 = !showContent004"></cell>
+            <template class="slide" :class="showContent004?'animate':''" v-if="showContent004">
+                <cell-box :border-intent="false" class="sub-item" is-link>个人</cell-box>
+                <cell-box class="sub-item" is-link>企业</cell-box>
+            </template>
+
+            <cell title="贷款" is-link :border-intent="false" :arrow-direction="showContent005 ? 'up' : 'down'" @click.native="showContent005 = !showContent005"></cell>
+            <template class="slide" :class="showContent005?'animate':''" v-if="showContent005">
+                <cell-box :border-intent="false" class="sub-item" is-link>个人</cell-box>
+                <cell-box class="sub-item" is-link>企业</cell-box>
+            </template>
+
+        </group>
+    </div>
+</template>
+<script>
+    import { Cell, CellBox, CellFormPreview, Group } from 'vux'
+    export default {
+        components: {
+            Group,
+            Cell,
+            CellFormPreview,
+            CellBox
+        },
+        data() {
+            return {
+                showContent001: false,
+                showContent002: false,
+                showContent003: false,
+                showContent004: false,
+                showContent005: false
+
+            }
+        }
+    }
+
+</script>
+<style>
+    .sub-item {
+        color: #888;
+    }
+
+    .slide {
+        padding: 0 20px;
+        overflow: hidden;
+        max-height: 0;
+        transition: max-height .5s cubic-bezier(0, 1, 0, 1) -.1s;
+    }
+
+    .animate {
+        max-height: 9999px;
+        transition-timing-function: cubic-bezier(0.5, 0, 1, 0);
+        transition-delay: 0s;
+    }
+</style>
